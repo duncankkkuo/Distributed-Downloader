90929d0d1a045766ad89dd6bb5f330aec8126ea7
duncankkkuo <kawhi0908@gmail.com>
2016-10-19 11:43:34 +0800

    add protocol FTP

--- DownloadServer.py
+++ DownloadServer.py
@@ -3,83 +3,132 @@ import json
 import traceback
 from kafka import KafkaConsumer, KafkaProducer
 import os
+import sys
+import ftplib
+import socket
 
-# Json
-with open('consumer.json', 'r') as reader:
-    json_get = json.loads(reader.read())
-
-# Connect to kafka
-broker = json_get['kafka_server']['broker']
-group = json_get['kafka_server']['group']
-consumer = KafkaConsumer(group_id = group,
-                         bootstrap_servers = broker)
-
-# Add all topics to topic group
-topic_all = []
-for topic in json_get['kafka_server']['topic']:
-    topic_id = json_get['kafka_server']['topic'][topic]
-    topic_all.append(topic_id)
-
-consumer.subscribe(topic_all)
-
-# Get the json from kafka
-for message in consumer:
-    kafka_json_get = json.loads(message.value)
-
-    # Json message get
-    if(kafka_json_get != None):
-        # Open transport
-        host = kafka_json_get['observe_target']['host']
-        port = kafka_json_get['observe_target']['port']
-        transport = paramiko.Transport((host, port))
-
-        # Auth
-        username = kafka_json_get['observe_target']['username']
-        password = kafka_json_get['observe_target']['password']
-        protocol = kafka_json_get['observe_target']['protocol']
-        file_dir_path = kafka_json_get['observe_target']['file_dir_path']
-        finish_dir_path = kafka_json_get['observe_target']['finish_dir_path']
-        local_dir_path = kafka_json_get['observe_target']['local_dir_path']
-        file_name = kafka_json_get['observe_target']['file_name']
-        file_size = kafka_json_get['observe_target']['file_size']
-        topic = kafka_json_get['observe_target']['topic']
-
-        file_path = file_dir_path + file_name
-        finish_path = finish_dir_path + file_name
-        local_path = local_dir_path + file_name
-        transport.connect(username=username, password=password)
-
-        # Sftp to observe_target
-        sftp = paramiko.SFTPClient.from_transport(transport)
-
-        #Check free disk size
-        statvfs = os.statvfs(local_dir_path)
-        free_size = statvfs.f_frsize * statvfs.f_bfree
-
-        # Download
+def connect(protocol, port, host, username, password):
+    if (protocol == 'sftp'):
         try:
+            paramiko.util.log_to_file('/tmp/paramiko.log')
+            transport = paramiko.Transport((host, port))
+            transport.connect(username=username, password=password)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            return sftp
+        except:
+            print("SFTP is unavailable,please check the host,username and password!")
+            sys.exit(0)
+
+    elif (protocol == 'ftp'):
+        try:
+            ftp = ftplib.FTP(host)
+            ftp.login(username, password)
+            return ftp
+        except socket.error, socket.gaierror:
+            print("FTP is unavailable,please check the host,username and password!")
+            sys.exit(0)
+
+def download_file(connect, protocol, file_dir_path, local_dir_path, file_name):
+    if (protocol == 'sftp'):
+        try:
+            file_path = file_dir_path + file_name
+            local_path = local_dir_path + file_name
+            connect.get(file_path, local_path)
+        except:
+            traceback.print_exc()
+
+    elif (protocol == 'ftp'):
+        try:
+            file_path = file_dir_path + file_name
+            local_file_name = os.path.join(local_dir_path, file_name)
+            file = open(local_file_name, 'wb')
+            connect.retrbinary('RETR '+ file_path, file.write)
+        except ftplib.error_perm, resp:
+            traceback.print_exc()
+
+def move_file(connect, protocol, file_path, finish_path):
+    if (protocol == 'sftp'):
+        try:
+            con.rename(file_path, finish_path)
+        except:
+            traceback.print_exc()
+
+    elif (protocol == 'ftp'):
+        try:
+            con.rename(file_path, finish_path)
+        except ftplib.error_perm, resp:
+            traceback.print_exc()
+
+def disconnect(connect, protocol):
+    if (protocol == 'sftp'):
+        connect.close()
+
+    elif (protocol == 'ftp'):
+        connect.quit()
+
+def main():
+    # Json
+    with open('consumer.json', 'r') as reader:
+        json_get = json.loads(reader.read())
+
+    # Connect to kafka
+    broker = json_get['kafka_server']['broker']
+    group = json_get['kafka_server']['group']
+    consumer = KafkaConsumer(group_id = group,
+                             bootstrap_servers = broker)
+
+    # Add all topics to topic group
+    topic_all = []
+    for topic in json_get['kafka_server']['topic']:
+        topic_id = json_get['kafka_server']['topic'][topic]
+        topic_all.append(topic_id)
+
+    consumer.subscribe(topic_all)
+
+    # Get the json from kafka
+    for message in consumer:
+        kafka_json_get = json.loads(message.value)
+
+        # Json message get
+        if(kafka_json_get != None):
+            # Auth
+            username = kafka_json_get['observe_target']['username']
+            password = kafka_json_get['observe_target']['password']
+            protocol = kafka_json_get['observe_target']['protocol']
+            file_dir_path = kafka_json_get['observe_target']['file_dir_path']
+            finish_dir_path = kafka_json_get['observe_target']['finish_dir_path']
+            local_dir_path = kafka_json_get['observe_target']['local_dir_path']
+            file_name = kafka_json_get['observe_target']['file_name']
+            file_size = kafka_json_get['observe_target']['file_size']
+            host = kafka_json_get['observe_target']['host']
+            port = kafka_json_get['observe_target']['port']
+            topic = kafka_json_get['observe_target']['topic']
+
+            file_path = file_dir_path + file_name
+            finish_path = finish_dir_path + file_name
+            local_path = local_dir_path + file_name
+
+            con = connect(protocol, port, host, username, password)
+
+            #Check free disk size
+            statvfs = os.statvfs(local_dir_path)
+            free_size = statvfs.f_frsize * statvfs.f_bfree
+
+            # Download
             if(free_size * 0.9 > file_size):
-                sftp.get(file_path, local_path)
+                download_file(con, protocol, file_dir_path, local_dir_path, file_name)
             else:
                 producer = KafkaProducer(bootstrap_servers=broker)
                 producer.send(topic, json.dumps(kafka_json_get).encode('ascii'))
 
-            print local_path, file_path
+            print file_path
 
-        except IOError as e:
-            if e.errno == 13:
-                print 'Permission denied'
+            # Move finished files to finish_path
+            #move_file(con, protocol, file_path, finish_path)
 
-        except (EOFError, KeyboardInterrupt):
-            print 'Server interrupt'
+            # Close
+            disconnect(con, protocol)
+            print 'Done'
 
-        except:
-            traceback.print_exc()
-
-        # Move finished files to finish_path
-        #sftp.rename(file_path, finish_path)
-
-        # Close
-        sftp.close()
-        transport.close()
-        print 'Done'
+if __name__ == "__main__":
+  main()
--- ObserveServer.py
+++ ObserveServer.py
@@ -5,107 +5,190 @@ import paramiko
 from datetime import datetime
 import json
 from kafka import KafkaConsumer, KafkaProducer
-
-paramiko.util.log_to_file('/tmp/paramiko.log')
-
-# Json
-with open('producer.json', 'r') as reader:
-    json_get = json.loads(reader.read())
-
-# Concatenate server string for port 9092
-kafka_server = ''
-for server in json_get['observe_target']:
-    kafka_server += json_get['observe_target'][server]['host'] + ':9092,'
-
-kafka_server = kafka_server[:len(kafka_server) - 1]
-
-# Sqlalchemy
-while(True):
-    for server in json_get['observe_target']:
-        # Open transport
-        host = json_get['observe_target'][server]['host']
-        port = json_get['observe_target'][server]['port']
-        transport = paramiko.Transport((host, port))
-
-        # Auth
-        username = json_get['observe_target'][server]['username']
-        password = json_get['observe_target'][server]['password']
-        protocol = json_get['observe_target'][server]['protocol']
-        file_dir_path = json_get['observe_target'][server]['file_dir_path']
-        finish_dir_path = json_get['observe_target'][server]['finish_dir_path']
-        local_dir_path = json_get['observe_target'][server]['local_dir_path']
-        topic = json_get['observe_target'][server]['topic']
-        sqlite_db = json_get['sqlite_db']['db_name']
-        transport.connect(username=username, password=password)
-
-        # Sftp to observe_target
-        sftp = paramiko.SFTPClient.from_transport(transport)
-
-        # Sqlalchemy
-        engine = create_engine('sqlite:///./'+sqlite_db, echo=True)
-        metadata = MetaData(engine)
-
-        Base = declarative_base()
-
-        class Server(Base):
-            __tablename__ = server
-
-            id = Column(Integer, primary_key=True)
-            name = Column(String)
-            date = Column(String)
-            size = Column(Integer)
-            kafka = Column(Integer)
-            sftp = Column(Integer)
-
-            def __init__(self, name, date, size):
-                self.name = name
-                self.date = date
-                self.size = size
-        #Check table exist
-        if not engine.dialect.has_table(engine, server):
-            Base.metadata.create_all(engine)
-
-        Session = sessionmaker()
-        Session.configure(bind=engine)
-        session = Session()
+import sys
+import os
+import ftplib
+import socket
 
 
-        # List file to sql
-        print sftp.listdir(file_dir_path)
-        for file_name in sftp.listdir(file_dir_path):
-            file_path = file_dir_path + file_name
-            mtime = sftp.stat(file_path).st_mtime
+def connect(protocol, port, host, username, password):
+    if (protocol == 'sftp'):
+        try:
+            paramiko.util.log_to_file('/tmp/paramiko.log')
+            transport = paramiko.Transport((host, port))
+            transport.connect(username=username, password=password)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            return sftp
+        except:
+            print("SFTP is unavailable,please check the host,username and password!")
+            sys.exit(0)
+
+    elif (protocol == 'ftp'):
+        try:
+            ftp = ftplib.FTP(host)
+            ftp.login(username, password)
+            return ftp
+        except socket.error, socket.gaierror:
+            print("FTP is unavailable,please check the host,username and password!")
+            sys.exit(0)
+
+
+def get_file_list(connect, protocol, file_dir_path):
+    if (protocol == 'sftp'):
+        files = []
+        try:
+            files = connect.listdir(file_dir_path)
+            return files
+        except:
+            print "No files in this directory"
+
+    elif (protocol == 'ftp'):
+        files = []
+        try:
+            for file_name in connect.nlst(file_dir_path):
+                files.append(file_name.replace(file_dir_path, ""))
+            return files
+        except ftplib.error_perm, resp:
+            if str(resp) == "550 No files found":
+                print "No files in this directory"
+
+
+def get_file_date(connect, protocol, file_path):
+    if (protocol == 'sftp'):
+        try:
+            mtime = connect.stat(file_path).st_mtime
             file_date = datetime.fromtimestamp(mtime)
-            file_size = sftp.stat(file_path).st_size
+            return file_date
+        except:
+            print "No files in this directory"
 
-            # Check filename exist in sql
-            if(session.query(Server).filter(Server.name == file_name).count() == 0):
-                consumer_json = {
-                    "observe_target": {
-                        "host": host,
-                        "username": username,
-                        "password": password,
-                        "protocol": protocol,
-                        "port": port,
-                        "file_dir_path": file_dir_path,
-                        "finish_dir_path": finish_dir_path,
-                        "local_dir_path": local_dir_path,
-                        "file_name": file_name,
-                        "file_size": file_size,
-                        "topic": topic
+    elif (protocol == 'ftp'):
+        try:
+            file_date = connect.sendcmd('MDTM ' + file_path)
+            return file_date
+        except ftplib.error_perm, resp:
+            if str(resp) == "550 No files found":
+                print "No files in this directory"
+
+def get_file_size(connect, protocol, file_path):
+    if (protocol == 'sftp'):
+        try:
+            file_size = connect.stat(file_path).st_size
+            return file_size
+        except:
+            print "No files in this directory"
+
+    elif (protocol == 'ftp'):
+        try:
+            file_size = connect.size(file_path)
+            return file_size
+        except ftplib.error_perm, resp:
+            if str(resp) == "550 No files found":
+                print "No files in this directory"
+
+def disconnect(connect, protocol):
+    if (protocol == 'sftp'):
+        connect.close()
+
+    elif (protocol == 'ftp'):
+        connect.quit()
+
+def main():
+    # Json
+    with open('producer.json', 'r') as reader:
+        json_get = json.loads(reader.read())
+
+    # Concatenate server string for port 9092
+    kafka_server = ''
+    for server in json_get['observe_target']:
+        kafka_server += json_get['observe_target'][server]['host'] + ':9092,'
+
+    kafka_server = kafka_server[:len(kafka_server) - 1]
+
+    # Sqlalchemy
+    while(True):
+        for server in json_get['observe_target']:
+
+            # Auth
+            host = json_get['observe_target'][server]['host']
+            port = json_get['observe_target'][server]['port']
+            username = json_get['observe_target'][server]['username']
+            password = json_get['observe_target'][server]['password']
+            protocol = json_get['observe_target'][server]['protocol']
+            file_dir_path = json_get['observe_target'][server]['file_dir_path']
+            finish_dir_path = json_get['observe_target'][server]['finish_dir_path']
+            local_dir_path = json_get['observe_target'][server]['local_dir_path']
+            topic = json_get['observe_target'][server]['topic']
+            sqlite_db = json_get['sqlite_db']['db_name']
+
+            con = connect(protocol, port, host, username, password)
+
+            # Sqlalchemy
+            engine = create_engine('sqlite:///./'+sqlite_db, echo=True)
+            metadata = MetaData(engine)
+
+            Base = declarative_base()
+
+            class Server(Base):
+                __tablename__ = server
+
+                id = Column(Integer, primary_key=True)
+                name = Column(String)
+                date = Column(String)
+                size = Column(Integer)
+                kafka = Column(Integer)
+                sftp = Column(Integer)
+
+                def __init__(self, name, date, size):
+                    self.name = name
+                    self.date = date
+                    self.size = size
+            # Check table exist
+            if not engine.dialect.has_table(engine, server):
+                Base.metadata.create_all(engine)
+
+            Session = sessionmaker()
+            Session.configure(bind=engine)
+            session = Session()
+
+            # List file to sql
+            file_list = get_file_list(con, protocol, file_dir_path)
+            for file_name in file_list:
+                file_path = file_dir_path + file_name
+                file_date = get_file_date(con, protocol, file_path)
+                file_size = get_file_size(con, protocol, file_path)
+
+                # Check filename exist in sql
+                if(session.query(Server).filter(Server.name == file_name).count() == 0):
+                    consumer_json = {
+                        "observe_target": {
+                            "host": host,
+                            "username": username,
+                            "password": password,
+                            "protocol": protocol,
+                            "port": port,
+                            "file_dir_path": file_dir_path,
+                            "finish_dir_path": finish_dir_path,
+                            "local_dir_path": local_dir_path,
+                            "file_name": file_name,
+                            "file_size": file_size,
+                            "topic": topic
+                        }
                     }
-                }
 
-                s = Server(file_name, file_date, file_size)
-                session.add(s)
+                    s = Server(file_name, file_date, file_size)
+                    session.add(s)
 
-                # Send message to kafka
-                producer = KafkaProducer(bootstrap_servers=kafka_server)
-                producer.send(topic, json.dumps(consumer_json).encode('ascii'))
+                    # Send message to kafka
+                    producer = KafkaProducer(bootstrap_servers=kafka_server)
+                    producer.send(topic, json.dumps(consumer_json).encode('ascii'))
 
-                session.commit()
+                    session.commit()
 
-        # Close
-        sftp.close()
-        transport.close()
-        print('Done')
+            # Close
+            disconnect(con, protocol)
+            print('Done')
+}
+
+if __name__ == "__main__":
+  main()
